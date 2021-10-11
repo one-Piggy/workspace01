92a331a066d634d9f06a40f706e27700fe1af1e5
Xb <Xb@atguigu.com>
2021-10-11 17:26:08 +0800

    master test commit

--- src/main/java/com/sgg/GitTest.java
+++ src/main/java/com/sgg/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello2222222");
         System.out.println("hello333333");
         System.out.println("hello4444444");
+        System.out.println("master  test");
     }
 }
