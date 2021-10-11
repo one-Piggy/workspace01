066f95d909eb0a05159773a5d7e93605eab46dbc
Xb <Xb@atguigu.com>
2021-10-11 16:59:55 +0800

    first commit

---
+++ src/main/java/com/sgg/GitTest.java
@@ -0,0 +1,13 @@
+package com.sgg;
+
+/**
+ * @program: workspace01
+ * @description:
+ * @author: xb
+ * @create: 2021-10-11 16:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好！！！！");
+    }
+}
